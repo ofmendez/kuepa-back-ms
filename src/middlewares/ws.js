927cde0a62c6f9b443ebd1a6459530d3ffd7d0bd
Fabian Mendez <ofmendez@gmail.com>
2025-01-30 04:16:25 -0500

    add keep-alive functionality to WebSocket connections

--- src/middlewares/ws.js
+++ src/middlewares/ws.js
@@ -1,7 +1,17 @@
 const clients = new Set();
+const KEEP_ALIVE_INTERVAL = 30000; // 30 seconds
 
 export const CreateWsEndpoint = ({ app, upgradeWebSocket, msgController }) => {
   app.get('/ws', upgradeWebSocket((c) => {
+    const keepAlive = (ws) => {
+      if (ws.readyState === WebSocket.OPEN)
+        ws.send(JSON.stringify({ type: 'ping' }));
+    };
+
+    const interval = setInterval(() => {
+      clients.forEach(keepAlive);
+    }, KEEP_ALIVE_INTERVAL);
+
     return {
       onMessage (event, ws) {
         const message = event.data;
@@ -16,6 +26,7 @@ export const CreateWsEndpoint = ({ app, upgradeWebSocket, msgController }) => {
       },
       onClose (ws) {
         clients.delete(ws);
+        clearInterval(interval);
         console.log('WebSocket connection closed');
       },
     };
